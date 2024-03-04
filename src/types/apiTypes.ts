3b7bc3948892ca208d825bc25d5a962b4224fe06
Hyunsoo_kim <red_panda119@naver.com>
2024-03-04 12:23:34 +0900

    feat: AuthProvider 구현, axios install

---
+++ src/types/apiTypes.ts
@@ -0,0 +1,65 @@
+type UserCookieType = {
+  id: number | null;
+  email: string | null;
+  nickname: string | null;
+  profileImageUrl: string | null;
+  accessToken: string | null;
+  refreshToken: string | null;
+  createdAt: string | null;
+  updatedAt: string | null;
+};
+
+type JoinResponse = {
+  id: number;
+  email: string;
+  nickname: string;
+  profileImageUrl: string;
+  createdAt: string;
+  updatedAt: string;
+};
+
+type LoginResponse = {
+  id: number;
+  email: string;
+  nickname: string;
+  profileImageUrl: string;
+  createdAt: string;
+  updatedAt: string;
+  refreshToken: string;
+  accessToken: string;
+};
+
+type JoinInfo = {
+  email: string;
+  nickname: string;
+  password: string;
+};
+
+type LoginInfo = {
+  email: string;
+  password: string;
+};
+
+type UserInfoToUpdate = {
+  nickname: string;
+  profileImageUrl: string;
+  newPassword: string;
+};
+
+type cookieCollection = {
+  accessToken?: string;
+  refreshToken?: string;
+  email?: string;
+  nickname?: string;
+  profileImageUrl?: string;
+};
+
+export type {
+  UserCookieType,
+  JoinResponse,
+  LoginResponse,
+  JoinInfo,
+  LoginInfo,
+  UserInfoToUpdate,
+  cookieCollection
+};
