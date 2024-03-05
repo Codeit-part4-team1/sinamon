be484660fdb22243583765db1994adeeb54ebfab
Hyunsoo_kim <red_panda119@naver.com>
2024-03-05 12:29:25 +0900

    feat: AuthProvider에 일부 함수에 에러 처리,리스폰스 객체 반환 추가

--- src/contexts/AuthProvider.tsx
+++ src/contexts/AuthProvider.tsx
@@ -1,10 +1,7 @@
 import { ReactNode, useState, useEffect, createContext } from "react";
-import { useRouter } from "next/router";
 import axios from "axios";
 import {
   UserCookieType,
-  // JoinResponse,
-  // LoginResponse,
   JoinInfo,
   LoginInfo,
   UserInfoToUpdate,
@@ -27,7 +24,6 @@ type AuthProviderProps = {
 };
 
 export default function AuthProvider({ children }: AuthProviderProps) {
-  const router = useRouter();
   const [isLoggedIn, SetIsLoggedIn] = useState<boolean>(false);
 
   //필요한 데이터 구조 분해 할당으로 받기.
@@ -42,54 +38,58 @@ export default function AuthProvider({ children }: AuthProviderProps) {
     updatedAt: null
   });
 
-  //회원 가입, 성공 시 로그인 페이지 이동
+  //회원 가입, 리스폰스 객체 반환
   async function join(joinInfo: JoinInfo) {
-    const res = await axios.post(
-      "https://sp-globalnomad-api.vercel.app/2-1/users",
-      joinInfo
-    );
-
-    router.push("/signIn");
-
-    return res;
+    try {
+      const res = await axios.post(
+        "https://sp-globalnomad-api.vercel.app/2-1/users",
+        joinInfo
+      );
+  
+      return res;
+    } catch(error) {
+      return error;
+    }
   }
 
-  //로그인, 성공 시 쿠키, useCookie객체에 리스폰스 데이터 저장 후 메인 페이지 이동
+  //로그인, 리스폰스 객체 반환, 로그인 성공 시 쿠키 탭에 쿠키 저장, userCookie에 값 할당, isLoggedIn값 true 할당
   async function login(loginInfo: LoginInfo) {
-    const res = await axios.post(
-      "https://sp-globalnomad-api.vercel.app/2-1/auth/login",
-      loginInfo
-    );
+    try {
+      const res = await axios.post(
+        "https://sp-globalnomad-api.vercel.app/2-1/auth/login",
+        loginInfo
+      );
 
-    document.cookie = `id=${res.data.user.id}`;
-    document.cookie = `email=${res.data.user.email}`;
-    document.cookie = `nickname=${res.data.user.nickname}`;
-    document.cookie = `profileImageUrl=${res.data.user.profileImageUrl}`; //"null"(string 타입)
-    document.cookie = `createdAt =${res.data.user.createdAt}`;
-    document.cookie = `updatedAt = ${res.data.user.updatedAt}`;
-    document.cookie = `accessToken=${res.data.accessToken}`; //JWT
-    document.cookie = `refreshToken=${res.data.refreshToken}`; //JWT
+      document.cookie = `id=${res.data.user.id}`;
+      document.cookie = `email=${res.data.user.email}`;
+      document.cookie = `nickname=${res.data.user.nickname}`;
+      document.cookie = `profileImageUrl=${res.data.user.profileImageUrl}`; //"null"(string 타입)
+      document.cookie = `createdAt =${res.data.user.createdAt}`;
+      document.cookie = `updatedAt = ${res.data.user.updatedAt}`;
+      document.cookie = `accessToken=${res.data.accessToken}`; //JWT
+      document.cookie = `refreshToken=${res.data.refreshToken}`; //JWT
 
-    setUserCookie((prev) => ({
-      ...prev,
-      id: res.data.user.id,
-      email: res.data.user.email,
-      nickname: res.data.user.nickname,
-      profileImageUrl: res.data.user.profileImageUrl,
-      createdAt: res.data.user.createdAt,
-      updatedAt: res.data.user.updatedAt,
-      accessToken: res.data.accessToken,
-      refreshToken: res.data.refreshToken
-    }));
+      setUserCookie((prev) => ({
+        ...prev,
+        id: res.data.user.id,
+        email: res.data.user.email,
+        nickname: res.data.user.nickname,
+        profileImageUrl: res.data.user.profileImageUrl,
+        createdAt: res.data.user.createdAt,
+        updatedAt: res.data.user.updatedAt,
+        accessToken: res.data.accessToken,
+        refreshToken: res.data.refreshToken
+      }));
 
-    SetIsLoggedIn(true);
+      SetIsLoggedIn(true);
 
-    // router.push("/");
-
-    return res;
+      return res;
+    } catch (error) {
+      return error;
+    }
   }
 
-  //로그아웃, 쿠키 값 삭제
+  //로그아웃, 쿠키 값 삭제, useCookie 값 초기화, isLoggedIn 값 false 할당
   async function logout() {
     deleteCookie("id");
     deleteCookie("accessToken");
@@ -114,33 +114,37 @@ export default function AuthProvider({ children }: AuthProviderProps) {
     SetIsLoggedIn(false);
   }
 
-  //유저 정보 수정
+  //유저 정보 수정, 리스폰스 객체 반환, 수정된 정보로 쿠키 값, useCookie 값 변경
   async function updateUserInfo(userInfoToEdit: UserInfoToUpdate) {
-    const res = await axios.put(
-      "https://sp-globalnomad-api.vercel.app/2-1/users/me",
-      userInfoToEdit,
-      { headers: { Authorization: `Bearer ${userCookie.accessToken}` } }
-    );
-
-    deleteCookie("email");
-    deleteCookie("nickname");
-    deleteCookie("profileImageUrl");
-    deleteCookie("updatedAt");
-
-    document.cookie = `email=${res.data.email}`;
-    document.cookie = `nickname=${res.data.nickname}`;
-    document.cookie = `profileImageUrl=${res.data.profileImageUrl}`;
-    document.cookie = `updatedAt=${res.data.updatedAt}`;
-
-    setUserCookie((prev: UserCookieType) => ({
-      ...prev,
-      email: res.data.email,
-      nickname: res.data.nickname,
-      profileImageUrl: res.data.profileImageUrl,
-      updatedAt: res.data.updatedAt
-    }));
-
-    return res;
+    try {
+      const res = await axios.put(
+        "https://sp-globalnomad-api.vercel.app/2-1/users/me",
+        userInfoToEdit,
+        { headers: { Authorization: `Bearer ${userCookie.accessToken}` } }
+      );
+  
+      deleteCookie("email");
+      deleteCookie("nickname");
+      deleteCookie("profileImageUrl");
+      deleteCookie("updatedAt");
+  
+      document.cookie = `email=${res.data.email}`;
+      document.cookie = `nickname=${res.data.nickname}`;
+      document.cookie = `profileImageUrl=${res.data.profileImageUrl}`;
+      document.cookie = `updatedAt=${res.data.updatedAt}`;
+  
+      setUserCookie((prev: UserCookieType) => ({
+        ...prev,
+        email: res.data.email,
+        nickname: res.data.nickname,
+        profileImageUrl: res.data.profileImageUrl,
+        updatedAt: res.data.updatedAt
+      }));
+  
+      return res;
+    } catch(error) {
+      return error;
+    }
   }
 
   //useEffect안에서만 사용
@@ -169,9 +173,6 @@ export default function AuthProvider({ children }: AuthProviderProps) {
       name + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
   }
 
-  console.log(userCookie);
-  console.log(isLoggedIn);
-
   return (
     <AuthContext.Provider
       value={{
@@ -190,4 +191,3 @@ export default function AuthProvider({ children }: AuthProviderProps) {
 }
 
 export { AuthContext };
-
