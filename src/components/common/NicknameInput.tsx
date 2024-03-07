585b8fa51f257ac2d9aa402e30bafe08678db121
Hyunsoo_kim <red_panda119@naver.com>
2024-03-07 12:08:55 +0900

    faet: 에러 수정, UI 구현 (미완성)

--- src/components/common/NicknameInput.tsx
+++ src/components/common/NicknameInput.tsx
@@ -5,15 +5,16 @@ type NicknameInputProps = {
   whatFor: "signUp" | "updateUserInfo";
   nickname: string;
   handlerOnChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  inspection: boolean;
+  inspection: boolean | undefined;
   setInspection: Dispatch<SetStateAction<InspectionType>>;
 };
 
 type InspectionType = {
-  email: boolean;
-  nickname: boolean;
-  password: boolean;
-  checkPassword: boolean;
+  email?: boolean;
+  nickname?: boolean;
+  profileImageUrl?: boolean;
+  password?: boolean;
+  checkPassword?: boolean;
 };
 
 export default function NicknameInput({
@@ -34,17 +35,22 @@ export default function NicknameInput({
   }
 
   return (
-    <div>
-      <label htmlFor="nickname">닉네임</label>
+    <div className="relative flex flex-col gap-2" v>
+      <label htmlFor="nickname" className="text-md font-medium">
+        닉네임
+      </label>
       <input
         name="nickname"
         placeholder="닉네임을 입력해 주세요"
         onChange={onChange}
         value={whatFor === "updateUserInfo" ? userCookie.nickname : nickname}
+        className="border border-gray-79747e rounded-md h-12 p-5"
       />
-      {nickname.length === 0 || inspection ? undefined : (
-        <small>잘못된 닉네임 입니다</small>
-      )}
+      <div className="h-4">
+        {nickname.length === 0 || inspection ? undefined : (
+          <small className="pl-3 text-red-500">잘못된 닉네임 입니다</small>
+        )}
+      </div>
     </div>
   );
 }
