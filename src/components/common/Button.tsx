79242c7b03eaedabe03dc252011de022d4bdb780
ymj0828 <96277798+ymj0828@users.noreply.github.com>
2024-03-08 12:56:36 +0900

    feat: className를 props로 받아올 수 있게 수정

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,27 +1,35 @@
 interface ButtonProps {
   text: string;
-  size: "full" | "lg" | "md" | "sm";
+  size?: "full" | "lg" | "md" | "sm" | "";
   type: "button" | "submit";
   status?: "default" | "inactive" | "second";
+  className?: string;
 }
-const Button = ({ text, size, type, status = "default" }: ButtonProps) => {
-  const sizeClass: { [key in ButtonProps["size"]]: string } = {
+const Button = ({
+  text,
+  size = "",
+  type,
+  status = "default",
+  className
+}: ButtonProps) => {
+  const sizeClass: { [key in NonNullable<ButtonProps["size"]>]: string } = {
     full: "w-full h-12 text-base",
     lg: "h-12 px-8 text-base",
     md: "h-10 px-6 text-base",
-    sm: "h-8 px-4 text-sm"
+    sm: "h-8 px-4 text-sm",
+    "": ""
   };
 
   const statusClass: { [key in NonNullable<ButtonProps["status"]>]: string } = {
     default:
-      "bg-main border border-main text-white-ffffff hover:bg-sub hover:text-main",
+      "bg-main border border-main text-white-ffffff hover:bg-sub hover:text-main hover:border-main",
     inactive: "bg-gray-a4a1aa text-white-ffffff",
     second: "bg-sub text-main hover:border hover:border-main"
   };
 
   return (
     <button
-      className={`${sizeClass[size]} ${statusClass[status]} rounded-md font-medium border border-transparent`}
+      className={`${sizeClass[size]} ${statusClass[status]} rounded-md font-medium border border-transparent ${className}`}
       type={type}
     >
       {text}
