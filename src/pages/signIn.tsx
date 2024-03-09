efabd03a62d33697c5d332473047ea38319952f3
Hyunsoo_kim <red_panda119@naver.com>
2024-03-09 11:04:05 +0900

    feat:SignInPage UI수정

--- src/pages/signIn.tsx
+++ src/pages/signIn.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect, useContext, FormEvent } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { AuthContext } from "@/contexts/AuthProvider";
 import EmailInput from "@/components/common/EmailInput";
 import PasswordInput from "@/components/common/PasswordInput";
@@ -19,9 +20,10 @@ type InspectionType = {
   checkPassword?: boolean;
 };
 
+
 export default function SignIn() {
+  const router = useRouter();
   const { userCookie, login } = useContext(AuthContext);
-  const [res, setRes] = useState();
   const [loginValue, setLoginValue] = useState<LoginInfo>({
     email: "",
     password: ""
@@ -48,49 +50,51 @@ export default function SignIn() {
 
   async function handlerSubmit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
-    const response = await login(loginValue);
-    setRes(response);
+
+    const res = await login(loginValue);
   }
 
   return (
-    <div className="flex-col gap-5 mx-auto mt-[200px] w-96 sm:w-[342px] md:w-[632px] lg:w-[640px]">
-      <div>
-        <Image
-          src="/images/logo.png"
-          alt="logo"
-          width={300}
-          height={100}
-          className="m-auto"
-        />
-      </div>
-      <form onSubmit={handlerSubmit} className="flex flex-col mt-10 gap-3">
+    <div className="relative w-screen h-screen pt-[150px] px-[12px]">
+      <div className="flex-col gap-5 mx-auto w-[350px] sm:w-[342px] md:w-[632px] lg:w-[640px] min-w-[375px]">
         <div>
-          <EmailInput
-            whatFor="login"
-            email={loginValue.email}
-            handlerOnChange={handlerOnChange}
-            inspection={inspection.email}
-            setInspection={setInspection}
+          <Image
+            src="/images/logo.png"
+            alt="logo"
+            width={300}
+            height={100}
+            className="m-auto"
           />
         </div>
-        <div>
-          <PasswordInput
-            whatFor="login"
-            password={loginValue.password}
-            handlerOnChange={handlerOnChange}
-            inspection={inspection.password}
-            setInspection={setInspection}
-          />
+        <form onSubmit={handlerSubmit} className="flex flex-col mt-10 gap-2">
+          <div>
+            <EmailInput
+              whatFor="login"
+              email={loginValue.email}
+              handlerOnChange={handlerOnChange}
+              inspection={inspection.email}
+              setInspection={setInspection}
+            />
+          </div>
+          <div>
+            <PasswordInput
+              whatFor="login"
+              password={loginValue.password}
+              handlerOnChange={handlerOnChange}
+              inspection={inspection.password}
+              setInspection={setInspection}
+            />
+          </div>
+          <div className="mt-7">
+            <Button text="로그인 하기" size="full" type="submit" />
+          </div>
+        </form>
+        <div className="flex justify-center gap-3 text-sm mx-auto mt-8">
+          <p>회원이 아니신가요?</p>
+          <Link href="signUp" className="underline">
+            회원가입하기
+          </Link>
         </div>
-        <div className="mt-7">
-          <Button text="로그인 하기" size="full" type="submit" />
-        </div>
-      </form>
-      <div className="flex justify-center gap-3 text-sm mx-auto mt-8">
-        <p>회원이 아니신가요?</p>
-        <Link href="signUp" className="underline">
-          회원가입하기
-        </Link>
       </div>
     </div>
   );
