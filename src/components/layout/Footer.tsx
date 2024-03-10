24b58b776d13fa41b4ad5dc518c4250cdc0b7ce0
Jinuk-develop <127473357+Jinuk-develop@users.noreply.github.com>
2024-03-10 19:25:16 +0900

    feat:footer icon 연결

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -9,25 +9,25 @@ export default function Footer() {
   return (
     <div className="px-9 pt-8 pb-16  md:justify-center md:items-center md:flex lg:flex md:gap-[60px] md:px-[123px] md:pb-[10] lg:gap-[300px] bg-primary">
       <div className="flex gap-3 justify-center pb-6 md:pb-0 md:gap-[60px] lg:gap-[300px] ">
-        <div className="text-center text-white-ffffff text-[16px] ">
+        <span className="text-center text-white-ffffff text-[16px] ">
           ©codeit - 2023
-        </div>
+        </span>
         <div className="flex items-center justify-center gap-[30px] text-[16px] text-white-ffffff">
           <span className="cursor-pointer">Privacy Policy</span>
           <span className="cursor-pointer">FAQ</span>
         </div>
       </div>
       <div className="flex items-center justify-center gap-3 text-lg md:m-0">
-        <Link href="/">
+        <Link href="https://facebook.com/">
           <FaFacebook className="text-white-ffffff" />
         </Link>
-        <Link href="/">
+        <Link href="https://twitter.com/">
           <FaTwitter className="text-white-ffffff" />
         </Link>
-        <Link href="/">
+        <Link href="https://youtube.com/">
           <FaYoutube className="text-white-ffffff" />
         </Link>
-        <Link href="/">
+        <Link href="https://instagram.com/">
           <FaInstagram className="text-white-ffffff" />
         </Link>
       </div>
