1e95fd2353ab6fb4aec488d8371c3d750b927362
ymj0828 <96277798+ymj0828@users.noreply.github.com>
2024-03-11 14:23:16 +0900

    chore: domains을 remotePatterns으로 변경

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,14 @@ const nextConfig = {
   images: {
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    domains: ["sprint-fe-project.s3.ap-northeast-2.amazonaws.com"]
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: '"sprint-fe-project.s3.ap-northeast-2.amazonaws.com',
+        port: "",
+        pathname: "/globalnomad/**"
+      }
+    ]
   },
   webpack: (config) => {
     config.module.rules.push({
