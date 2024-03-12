9c218aeb1ed5e50c54de35e68507e707244a2b04
Hyunsoo_kim <red_panda119@naver.com>
2024-03-12 13:20:55 +0900

    feat: 리액트 훅 폼 적용

--- src/components/common/AuthInput/CheckPasswordInput.tsx
+++ src/components/common/AuthInput/CheckPasswordInput.tsx
@@ -1,84 +1,59 @@
-import React, { Dispatch, SetStateAction, useState } from "react";
+import React, { useState } from "react";
 import { BsEye, BsEyeSlash } from "react-icons/bs";
 
-type CheckPasswordInputProps = {
-  whatFor: "signUp" | "updateUserInfo";
-  password: string;
-  checkPassword: string;
-  handlerOnChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  inspection: boolean | undefined;
-  setInspection: Dispatch<SetStateAction<InspectionType>>;
-};
+const CheckPasswordInput = ({ whatFor, errors, watch, register }: any) => {
+  const [eyesIcon, setEyesIcon] = useState<boolean>(false);
 
-type InspectionType = {
-  email?: boolean;
-  nickname?: boolean;
-  profileImageUrl?: boolean;
-  password?: boolean;
-  checkPassword?: boolean;
-};
-
-export default function CheckPasswordInput({
-  whatFor,
-  password,
-  checkPassword,
-  handlerOnChange,
-  inspection,
-  setInspection
-}: CheckPasswordInputProps) {
-  const [showEyesIcon, setShowEyesIcon] = useState<boolean>(false);
-
-  function toggleShowEyesIcon() {
-    setShowEyesIcon(!showEyesIcon);
-  }
-
-  function onChange(e: React.ChangeEvent<HTMLInputElement>) {
-    handlerOnChange(e);
-
-    e.target.value === password
-      ? setInspection((prev: InspectionType) => ({
-          ...prev,
-          checkPassword: true
-        }))
-      : setInspection((prev: InspectionType) => ({
-          ...prev,
-          checkPassword: false
-        }));
+  function togglesEyesIcon() {
+    setEyesIcon(!eyesIcon);
   }
 
   return (
-    <div className="relative flex flex-col gap-2">
+    <div className="relative flex flex-col gap-1">
       <label htmlFor="checkPassword" className="text-md font-medium">
         {whatFor === "signUp" ? "비밀번호 확인" : "비밀번호 재입력"}
       </label>
       <input
         name="checkPassword"
-        type={showEyesIcon ? "text" : "password"}
         placeholder="비밀번호를 한번 더 입력해 주세요"
-        onChange={onChange}
-        className={`border rounded-md h-12 p-5 ${
-          checkPassword.length === 0 || inspection
+        autoComplete="off"
+        type={eyesIcon ? "text" : "password"}
+        {...register("checkPassword", {
+          required: "비밀번호를 확인해 주세요",
+          validate: (field: string) => {
+            return !field
+              ? "비밀번호를 확인해 주세요"
+              : field.length > 0 && field === watch("password")
+                ? undefined
+                : "비밀번호가 일치하지 않습니다";
+          }
+        })}
+        className={`border rounded-md h-[48px] p-5 w-full focus:outline-none ${
+          watch("checkPassword")?.length === 0 || !errors.checkPassword
             ? "border-input"
             : "border-red-500"
         }`}
       />
       <div
-        onClick={toggleShowEyesIcon}
-        className="absolute right-5 bottom-10 hover:cursor-pointer"
+        onClick={togglesEyesIcon}
+        className="absolute right-5 bottom-9 hover:cursor-pointer"
       >
-        {showEyesIcon ? (
+        {eyesIcon ? (
           <BsEye width={25} height={23} />
         ) : (
           <BsEyeSlash width={25} height={23} />
         )}
       </div>
       <div className="h-4">
-        {checkPassword.length === 0 || inspection ? undefined : (
+        {errors.checkPassword && (
           <small className="pl-3 text-red-500">
-            비밀번호가 일치하지 않습니다
+            {errors.checkPassword.message}
           </small>
         )}
       </div>
     </div>
   );
-}
+};
+
+export default CheckPasswordInput;
+
