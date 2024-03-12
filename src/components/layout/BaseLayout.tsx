a9680c9e13f0af05ba9c246cee38acf0a053db44
ymj0828 <96277798+ymj0828@users.noreply.github.com>
2024-03-12 21:13:56 +0900

    feat: MenuLayout 구현

--- src/components/layout/BaseLayout.tsx
+++ src/components/layout/BaseLayout.tsx
@@ -10,7 +10,7 @@ const BaseLayout = ({ children }: BaseLayoutProps) => {
   return (
     <>
       <Header />
-      <main className="px-4 md:px-6">
+      <main className="pt-5 md:pt-7 px-4 md:px-6">
         <div className="max-w-screen-lg mx-auto mb-36 md:mb-48">{children}</div>
       </main>
       <Footer />
