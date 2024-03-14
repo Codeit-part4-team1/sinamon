9c9ba26f6cd620e62083d18e0b7536093ae15eb4
Hyunsoo_kim <red_panda119@naver.com>
2024-03-14 10:24:25 +0900

    브랜치이름변경,UI수정

--- src/pages/myInfo.tsx
+++ src/pages/myInfo.tsx
@@ -1,3 +1,8 @@
+import type { ReactElement } from "react";
+import type { NextPageWithLayout } from "@/pages/_app";
+import BaseLayout from "@/components/layout/BaseLayout";
+import MenuLayout from "@/components/layout/MenuLayout";
+
 import React, { useState, useContext } from "react";
 import { useForm } from "react-hook-form";
 import { HiPlus } from "react-icons/hi";
@@ -9,15 +14,13 @@ import PasswordInput from "@/components/common/AuthInput/PasswordInput";
 import CheckPasswordInput from "@/components/common/AuthInput/CheckPasswordInput";
 import AlertModal from "@/components/common/AlertModal";
 import Button from "@/components/common/Button";
-import Header from "@/components/layout/Header";
-import Footer from "@/components/layout/Footer";
 
 type ModalType = {
   modal: boolean;
   message: string;
 };
 
-const EditMyInfo = () => {
+const MyInfo: NextPageWithLayout = () => {
   const { updateUserInfo } = useContext(AuthContext);
   const {
     register,
@@ -47,10 +50,8 @@ const EditMyInfo = () => {
     }
   };
 
-  console.log(watch());
   return (
-    <div className="relative w-screen h-screen min-w-[420px]">
-      <Header />
+    <div className="relative max-w-[930px]">
       {modal.modal && (
         <AlertModal
           type="decide"
@@ -61,17 +62,14 @@ const EditMyInfo = () => {
           }}
         />
       )}
-      <div className="lg:flex flex-row lg:justify-between lg:gap-[200px] lg:w-[1230px] mx-auto">
-        <div className="sm:hidden md:hidden lg:block lg:pt-[40px]">
-          <Usermenu />
-        </div>
-        <div className="flex-col gap-5 mx-auto pb-[130px] w-[375px] md:w-[736px] md:pt-[40px] lg:w-[800px]">
+      <div className="mx-auto sm:pt-[20px] md:pt-[50px]">
+        <div className="flex-col gap-5 mx-auto">
           <form
             onSubmit={handleSubmit(submit.onSubmit, submit.onError)}
-            className="flex flex-col gap-5 pt-[50px] md:gap-10 md:pt-0"
+            className="flex flex-col gap-5 md:gap-10"
           >
             <h1 className="text-[28px] font-bold">내 정보</h1>
-            <div className="flex flex-col items-center gap-10 pt-[20px] md:flex-row md:pt-0">
+            <div className="flex flex-col items-center gap-10 sm:pt-[15px] md:pt-[0px] md:flex-row">
               <div className="relative flex flex-col gap-5 items-center">
                 <input
                   type="file"
@@ -84,7 +82,7 @@ const EditMyInfo = () => {
                 />
                 <Button text="초기화" size="full" type="button"></Button>
               </div>
-              <div className="relative flex flex-col gap-5 w-full md:justify-end md:translate-y-[18px]">
+              <div className="relative flex flex-col gap-5 md:translate-y-[18px] w-full">
                 <div>
                   <NicknameInput
                     whatFor="edit"
@@ -103,7 +101,7 @@ const EditMyInfo = () => {
                 </div>
               </div>
             </div>
-            <div className="relative flex flex-col gap-5 w-full">
+            <div className="relative flex flex-col gap-5">
               <div>
                 <PasswordInput
                   whatFor="edit"
@@ -127,9 +125,16 @@ const EditMyInfo = () => {
           </form>
         </div>
       </div>
-      <Footer />
     </div>
   );
 };
 
-export default EditMyInfo;
+MyInfo.getLayout = function getLayout(page: ReactElement) {
+  return (
+    <BaseLayout>
+      <MenuLayout>{page}</MenuLayout>
+    </BaseLayout>
+  );
+};
+
+export default MyInfo;
