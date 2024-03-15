b85d49459bdc13d3dfab8c3473d5f7cf6f7826a1
ymj0828 <96277798+ymj0828@users.noreply.github.com>
2024-03-15 22:38:51 +0900

    feat: 주소 및 일정 등록 기능 구현

--- src/pages/create.tsx
+++ src/pages/create.tsx
@@ -1,8 +1,9 @@
-import type { ReactElement } from "react";
+import React, { useState, type ReactElement } from "react";
 import type { NextPageWithLayout } from "@/pages/_app";
-import { useForm } from "react-hook-form";
+import { Controller, useForm, useFieldArray } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import DaumPostcode from "react-daum-postcode";
 import BaseLayout from "@/components/layout/BaseLayout";
 import MenuLayout from "@/components/layout/MenuLayout";
 import {
@@ -21,7 +22,11 @@ import {
   SelectTrigger,
   SelectValue
 } from "@/components/ui/select";
+import { Dialog, DialogTrigger, DialogContent } from "@/components/ui/dialog";
 import Button from "@/components/common/Button";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { FaPlus, FaMinus } from "react-icons/fa6";
 
 const priceNumberRegex = new RegExp("^[0-9]*$");
 const priceHeadZeroRegex = new RegExp("^(0|[1-9][0-9]*)$");
@@ -46,16 +51,26 @@ const formSchema = z.object({
     .max(255, {
       message: "설명은 256자 미만으로 입력해 주세요"
     }),
-  address: z.string()
+  address: z.object({
+    address1: z.string(),
+    address2: z.string(),
+    address3: z.string()
+  }),
+  datePicker: z.date(),
+  startTimePicker: z.date().or(z.string()),
+  endTimePicker: z.date().or(z.string()),
+  dateField: z.array(
+    z.object({
+      date: z.string(),
+      startTime: z.date().or(z.string()),
+      endTime: z.date().or(z.string())
+    })
+  )
 });
 
-function onSubmit(values: z.infer<typeof formSchema>) {
-  // Do something with the form values.
-  // ✅ This will be type-safe and validated.
-  console.log(values);
-}
-
 const CreatePage: NextPageWithLayout = () => {
+  const [dialogopen, setDialogOpen] = useState(false);
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -63,9 +78,42 @@ const CreatePage: NextPageWithLayout = () => {
       category: "문화 · 예술",
       price: "",
       description: "",
-      address: ""
+      address: {
+        address1: "",
+        address2: "",
+        address3: ""
+      },
+      datePicker: new Date(),
+      // startTimePicker: new Date("2024-12-17T24:00:00"),
+      startTimePicker: "",
+      endTimePicker: ""
+      // dateField: [{ date: "", date2: "" }]
     }
   });
+
+  const handleComplete = (data: any) => {
+    setDialogOpen((prev) => !prev);
+    form.setValue("address.address1", data.zonecode);
+    form.setValue("address.address2", data.address);
+    const addressValue = form.getValues("address");
+  };
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
+  const addressStyle = {
+    height: "500px"
+  };
+
+  const { control, register } = form;
+  const { fields, remove, append } = useFieldArray({
+    control,
+    name: "dateField"
+  });
+
   return (
     <>
       <p className="text-2xl md:text-3xl font-bold mb-5 md:mb-8">모임 등록</p>
@@ -198,38 +246,68 @@ const CreatePage: NextPageWithLayout = () => {
             />
           </div>
           <div className="max-w-[700px]">
-            <div>
-              <FormField
-                control={form.control}
-                name="address"
-                render={({ field }) => (
-                  <FormItem className="flex-1">
-                    <FormLabel className="text-lg md:text-xl font-semibold">
-                      주소
-                    </FormLabel>
-                    <div className="flex flex-col gap-2">
+            <div className="flex flex-col gap-2">
+              <Dialog open={dialogopen} onOpenChange={setDialogOpen}>
+                <DialogContent>
+                  <DaumPostcode
+                    style={addressStyle}
+                    onComplete={handleComplete}
+                  ></DaumPostcode>
+                </DialogContent>
+                <FormField
+                  control={form.control}
+                  name="address.address1"
+                  render={({ field }) => (
+                    <FormItem className="flex-1">
+                      <FormLabel className="text-lg md:text-xl font-semibold">
+                        주소
+                      </FormLabel>
                       <div className="flex items-end gap-2 md:gap-4">
+                        <DialogTrigger asChild>
+                          <FormControl>
+                            <Input
+                              className="h-10 md:h-12 px-3 md:px-4 mt-1 md:mt-2 text-sm md:text-base bg-white-ffffff border-gray-a4a1aa cursor-pointer"
+                              placeholder="우편번호"
+                              {...field}
+                            />
+                          </FormControl>
+                        </DialogTrigger>
+                        <FormMessage />
+                        <DialogTrigger asChild>
+                          <button
+                            className="flex-shrink-0 h-10 px-6 text-base md:h-12 md:px-8 bg-main text-white-ffffff hover:bg-sub hover:text-main hover:border-main rounded-md font-medium border border-transparent"
+                            type="button"
+                          >
+                            주소 검색
+                          </button>
+                        </DialogTrigger>
+                      </div>
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="address.address2"
+                  render={({ field }) => (
+                    <FormItem className="flex-1">
+                      <DialogTrigger asChild>
                         <FormControl>
                           <Input
-                            className="h-10 md:h-12 px-3 md:px-4 mt-1 md:mt-2 text-sm md:text-base bg-white-ffffff border-gray-a4a1aa"
-                            placeholder="우편번호"
+                            className="h-10 md:h-12 px-3 md:px-4 mt-1 md:mt-2 text-sm md:text-base bg-white-ffffff border-gray-a4a1aa cursor-pointer"
+                            placeholder="기본 주소를 입력해주세요"
                             {...field}
                           />
                         </FormControl>
-                        <FormMessage />
-                        <Button
-                          text="주소 검색"
-                          type="button"
-                          className="flex-shrink-0 h-10 px-6 text-base md:h-12 md:px-8"
-                        />
-                      </div>
-                      <FormControl>
-                        <Input
-                          className="h-10 md:h-12 px-3 md:px-4 mt-1 md:mt-2 text-sm md:text-base bg-white-ffffff border-gray-a4a1aa"
-                          placeholder="기본 주소를 입력해주세요"
-                          {...field}
-                        />
-                      </FormControl>
+                      </DialogTrigger>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="address.address3"
+                  render={({ field }) => (
+                    <FormItem className="flex-1">
                       <FormControl>
                         <Input
                           className="h-10 md:h-12 px-3 md:px-4 mt-1 md:mt-2 text-sm md:text-base bg-white-ffffff border-gray-a4a1aa"
@@ -238,11 +316,152 @@ const CreatePage: NextPageWithLayout = () => {
                         />
                       </FormControl>
                       <FormMessage />
-                    </div>
-                  </FormItem>
-                )}
-              />
+                    </FormItem>
+                  )}
+                />
+              </Dialog>
             </div>
+          </div>
+          <div className="flex flex-col gap-y-1 md:gap-y-2">
+            <span className="text-lg md:text-xl font-semibold">모임 일정</span>
+            <div className="flex items-end gap-x-2 md:gap-x-5">
+              <div className="flex flex-col">
+                <label className="text-md md:text-lg font-semibold">날짜</label>
+                <Controller
+                  name="datePicker"
+                  control={form.control}
+                  render={({ field: { onChange, value } }) => (
+                    <DatePicker
+                      className="w-[136px] md:w-40 h-10 md:h-12 px-3 md:px-4 mt-1 md:mt-2 text-sm md:text-base bg-white-ffffff border border-gray-a4a1aa rounded-md outline-none"
+                      dateFormat="yyyy년 MM월 dd일"
+                      dateFormatCalendar="yyyy년 MM월"
+                      shouldCloseOnSelect
+                      minDate={new Date()}
+                      selected={value}
+                      onChange={(data: any) => onChange(data)}
+                    />
+                  )}
+                />
+              </div>
+              <div className="flex flex-col">
+                <label className="text-md md:text-lg font-semibold">
+                  시작 시간
+                </label>
+                <Controller
+                  name="startTimePicker"
+                  control={form.control}
+                  render={({ field: { onChange, value } }) => (
+                    <DatePicker
+                      className="w-16 md:w-24 h-10 md:h-12 px-3 md:px-4 mt-1 md:mt-2 text-sm md:text-base bg-white-ffffff border border-gray-a4a1aa rounded-md outline-none"
+                      selected={value}
+                      onChange={(data: any) => onChange(data)}
+                      showTimeSelect
+                      showTimeSelectOnly
+                      timeIntervals={30}
+                      timeCaption="Time"
+                      timeFormat="HH:mm"
+                      dateFormat="HH:mm"
+                      minTime={new Date("2024-12-17T24:00:00")}
+                      maxTime={
+                        new Date(form.watch("endTimePicker")).setMinutes(
+                          new Date(form.watch("endTimePicker")).getMinutes() -
+                            10
+                        ) || new Date("2024-12-17T23:30:00")
+                      }
+                    />
+                  )}
+                />
+              </div>
+              <div className="flex flex-col">
+                <label className="text-md md:text-lg font-semibold">
+                  종료 시간
+                </label>
+                <Controller
+                  name="endTimePicker"
+                  control={form.control}
+                  render={({ field: { onChange, value } }) => (
+                    <DatePicker
+                      className="w-16 md:w-24 h-10 md:h-12 px-3 md:px-4 mt-1 md:mt-2 text-sm md:text-base bg-white-ffffff border border-gray-a4a1aa rounded-md outline-none"
+                      selected={value}
+                      onChange={(data: any) => onChange(data)}
+                      showTimeSelect
+                      showTimeSelectOnly
+                      timeIntervals={30}
+                      timeCaption="Time"
+                      timeFormat="HH:mm"
+                      dateFormat="HH:mm"
+                      minTime={
+                        new Date(form.watch("startTimePicker")).setMinutes(
+                          new Date(form.watch("startTimePicker")).getMinutes() +
+                            10
+                        ) || new Date("2024-12-17T24:00:00")
+                      }
+                      maxTime={new Date("2024-12-17T23:30:00")}
+                    />
+                  )}
+                />
+              </div>
+              <button
+                className="h-10 md:h-12 aspect-square flex justify-center items-center bg-main rounded-md"
+                type="button"
+                onClick={() => {
+                  append({
+                    date: `${form
+                      .getValues("datePicker")
+                      .toLocaleDateString("ko-KR", {
+                        year: "numeric",
+                        month: "long",
+                        day: "numeric"
+                      })}`,
+                    startTime: `${form
+                      .getValues("startTimePicker")
+                      .toLocaleString("en-US", {
+                        hour: "numeric",
+                        minute: "numeric",
+                        hour12: false
+                      })}`,
+                    endTime: `${form
+                      .getValues("endTimePicker")
+                      .toLocaleString("en-US", {
+                        hour: "numeric",
+                        minute: "numeric",
+                        hour12: false
+                      })}`
+                  });
+                }}
+              >
+                <FaPlus className="text-white-ffffff text-xl md:text-2xl" />
+              </button>
+            </div>
+            <ul>
+              {fields.map((item, index) => {
+                return (
+                  <li key={item.id}>
+                    <div className="flex items-end gap-x-2 md:gap-x-5">
+                      <Input
+                        className="w-[136px] md:w-40 h-10 md:h-12 px-3 md:px-4 mt-1 md:mt-2 text-sm md:text-base bg-white-ffffff border-gray-a4a1aa"
+                        {...register(`dateField.${index}.date`)}
+                      />
+                      <Input
+                        className="w-16 md:w-24 h-10 md:h-12 px-3 md:px-4 mt-1 md:mt-2 text-sm md:text-base bg-white-ffffff border-gray-a4a1aa"
+                        {...register(`dateField.${index}.startTime`)}
+                      />
+                      <Input
+                        className="w-16 md:w-24 h-10 md:h-12 px-3 md:px-4 mt-1 md:mt-2 text-sm md:text-base bg-white-ffffff border-gray-a4a1aa"
+                        {...register(`dateField.${index}.endTime`)}
+                      />
+                      <button
+                        className="h-10 md:h-12 aspect-square flex justify-center items-center bg-main rounded-md"
+                        type="button"
+                        onClick={() => remove(index)}
+                      >
+                        <FaMinus className="text-white-ffffff text-xl md:text-2xl" />
+                      </button>
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
           </div>
           <Button
             text="등록하기"
