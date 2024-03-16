a2c5ab92fe699c91f2dbb7f1cab5e9f95bc4c6a4
ymj0828 <96277798+ymj0828@users.noreply.github.com>
2024-03-16 14:29:31 +0900

    feat: type 변경

--- src/pages/create.tsx
+++ src/pages/create.tsx
@@ -339,7 +339,7 @@ const CreatePage: NextPageWithLayout = () => {
                       dateFormatCalendar="yyyy년 MM월"
                       shouldCloseOnSelect
                       minDate={new Date()}
-                      selected={value}
+                      selected={new Date(value)}
                       onChange={(data: any) => onChange(data)}
                     />
                   )}
@@ -384,7 +384,7 @@ const CreatePage: NextPageWithLayout = () => {
                   render={({ field: { onChange, value } }) => (
                     <DatePicker
                       className="w-16 md:w-24 h-10 md:h-12 px-3 md:px-4 mt-[6px] md:mt-2 text-sm md:text-base bg-white-ffffff border border-gray-a4a1aa rounded-md outline-none"
-                      selected={value}
+                      selected={new Date(value)}
                       onChange={(data: any) => onChange(data)}
                       showTimeSelect
                       showTimeSelectOnly
