7fbb838c5da5aa260a1dce0de9457d4fe884c0c2
ymj0828 <96277798+ymj0828@users.noreply.github.com>
2024-03-24 02:54:01 +0900

    style: outline 삭제 및 폰트 body에 설정

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,11 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { pretendard } from "@/pages/_app";
 
 export default function Document() {
   return (
     <Html lang="ko" suppressHydrationWarning>
       <Head />
-      <body className="relative">
+      <body className={`relative ${pretendard.className}`}>
         <Main />
         <NextScript />
         <div id="modal-root"></div>
