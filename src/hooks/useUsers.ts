d7e4239b8d3f24fb02b9736becbd43e04651fe3b
Hyunsoo_kim <red_panda119@naver.com>
2024-03-30 05:18:59 +0900

    feat: 내 정보 페이지 기능 구현, 관련 타입 설정

--- src/hooks/useUsers.ts
+++ src/hooks/useUsers.ts
@@ -3,6 +3,7 @@ import { useMutation } from "@tanstack/react-query";
 
 import { instance } from "@/lib/axios";
 import { SignUpRequest, SignUpModal } from "@/types/auth";
+import { MyInfoRequest, MyInfoModal } from "@/types/users";
 
 export const useUsers = {
   get: () =>
@@ -11,7 +12,7 @@ export const useUsers = {
       queryFn: () => instance.get("/users/me")
     }),
 
-  signUp: (modal: any, setModal: any) =>
+  signUp: (setModal: any, successDialog: any, failDialog: any) =>
     useMutation({
       mutationFn: (value: SignUpRequest) => instance.post("/users", value),
       onSuccess: () => {
@@ -19,14 +20,51 @@ export const useUsers = {
           ...prev,
           message: "가입이 완료되었습니다!"
         }));
-        modal.success.current.showModal();
+        successDialog.current.showModal();
       },
       onError: (err: any) => {
         setModal((prev: SignUpModal) => ({
           ...prev,
           message: err.response.data.message
         }));
+        failDialog.current.showModal();
+      }
+    }),
+  edit: (modal: MyInfoModal, setModal: any) =>
+    useMutation({
+      mutationFn: (value: MyInfoRequest) => {
+        console.log(value);
+        return instance.patch("/users/me", value);
+      },
+      onSuccess: () => {
+        setModal((prev: MyInfoModal) => ({
+          ...prev,
+          message: "수정되었습니다"
+        }));
+        modal.success.current.showModal();
+      },
+      onError: (err: any) => {
+        setModal((prev: MyInfoModal) => ({
+          ...prev,
+          message: "프로필 이미지를 등록해 주세요"
+        }));
         modal.fail.current.showModal();
+      }
+    }),
+  createImageUrl: (setValue: any) =>
+    useMutation({
+      mutationFn: (value: any) =>
+        instance.post("/users/me/image", value, {
+          headers: {
+            "Content-Type": "multipart/form-data"
+          }
+        }),
+      onSuccess: (response: any) => {
+        console.log(response);
+        setValue("profileImageUrl", response.data.profileImageUrl);
+      },
+      onError: (err: any) => {
+        console.log(err);
       }
     })
 };
