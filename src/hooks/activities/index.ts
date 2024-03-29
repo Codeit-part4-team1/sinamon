a409f1fe5451fe05c92c8b455c3fea011c3c4b37
ymj0828 <96277798+ymj0828@users.noreply.github.com>
2024-03-29 13:27:52 +0900

    feat: hook 분리

--- src/hooks/activities/index.ts
+++ src/hooks/activities/index.ts
@@ -1,8 +1,32 @@
-import { getAcitivity } from "@/api/activities";
-import { queryKey } from "@/constants/queryKeys";
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 
-export const useGetActivity = useQuery({
-  queryKey: [queryKey.activity],
-  queryFn: (id) => getAcitivity(id)
-});
+import { activities, getAcitivity } from "@/api/activities";
+import { queryKey } from "@/constants/queryKeys";
+
+// export const useGetActivity = useQuery({
+//   queryKey: [queryKey.activity],
+//   queryFn: (id) => getAcitivity(id)
+// });
+
+export const useActivities = {
+  getActivitiesList: (
+    method: string,
+    page: number,
+    size: number,
+    selectedCategory: string | null,
+    sort: string
+  ) =>
+    useInfiniteQuery({
+      queryKey: queryKey.getActivitiesList(selectedCategory, sort, page, size),
+      initialPageParam: 1,
+      queryFn: () =>
+        activities.getActivitiesList(
+          method,
+          sort,
+          page,
+          size,
+          selectedCategory
+        ),
+      getNextPageParam: (lastPage) => lastPage.cursorId
+    })
+};
