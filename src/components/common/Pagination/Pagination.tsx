adb496567883120e15536a557458baabf910c0ec
ymj0828 <96277798+ymj0828@users.noreply.github.com>
2024-04-01 13:35:18 +0900

    feat: 페이지네이션 6이상일 때 숫자가 가운데로 오게 하기

--- src/components/common/Pagination/Pagination.tsx
+++ src/components/common/Pagination/Pagination.tsx
@@ -4,12 +4,12 @@ import { cn } from "@/lib/utils";
 
 interface PaginationProps {
   totalPages?: any;
-  selectPage?: number;
+  selectPage: number;
   setSelectPage?: any;
-  canGoNext?: boolean
-  canGoPrev?: boolean
-  goNext?: () => void
-  goPrev?: () => void
+  canGoNext?: boolean;
+  canGoPrev?: boolean;
+  goNext?: () => void;
+  goPrev?: () => void;
 }
 
 const Pagination = ({
@@ -19,26 +19,33 @@ const Pagination = ({
   canGoNext,
   canGoPrev,
   goNext,
-  goPrev,
+  goPrev
 }: PaginationProps) => {
+  const pageCountToShow = 5;
   const pageNumbers = Array.from(
     { length: totalPages },
     (_, index) => index + 1
   );
 
+  const middlePage = Math.ceil(pageCountToShow / 2);
+  let startRange = Math.max(1, selectPage - middlePage + 1);
+  const endRange = Math.min(totalPages, startRange + pageCountToShow - 1);
+
+  startRange = Math.max(1, endRange - pageCountToShow + 1);
+
   return (
     <nav className="w-fit mx-auto">
       <ul className="flex gap-2">
         <li>
-          <button 
-            className="w-8 md:w-12 h-8 md:h-12 flex justify-center items-center bg-white-ffffff border border-gray-adaeb8 hover:border-gray-79747e rounded-md"
+          <button
+            className="group w-8 md:w-12 h-8 md:h-12 flex justify-center items-center bg-white-ffffff border border-gray-adaeb8 hover:border-gray-79747e rounded-md disabled:border-gray-dddddd dark:disabled:border-gray-4b4b4b"
             disabled={!canGoPrev}
             onClick={goPrev}
           >
-            <IoIosArrowBack className="w-4 md:w-5 h-4 md:h-5 text-gray-a4a1aa" />
+            <IoIosArrowBack className="w-4 md:w-5 h-4 md:h-5 text-gray-a4a1aa group-disabled:text-gray-dddddd dark:group-disabled:text-gray-4b4b4b" />
           </button>
         </li>
-        {pageNumbers.map((pageNumber) => (
+        {pageNumbers.slice(startRange - 1, endRange).map((pageNumber) => (
           <li key={pageNumber}>
             <button
               onClick={() => setSelectPage(pageNumber)}
@@ -52,12 +59,12 @@ const Pagination = ({
           </li>
         ))}
         <li>
-          <button 
-            className="w-8 md:w-12 h-8 md:h-12 flex justify-center items-center bg-white-ffffff border border-gray-adaeb8 hover:border-gray-79747e rounded-md"
+          <button
+            className="group w-8 md:w-12 h-8 md:h-12 flex justify-center items-center bg-white-ffffff border border-gray-adaeb8 hover:border-gray-79747e rounded-md disabled:border-gray-dddddd dark:disabled:border-gray-4b4b4b"
             disabled={!canGoNext}
             onClick={goNext}
           >
-            <IoIosArrowForward className="w-4 md:w-5 h-4 md:h-5 text-gray-a4a1aa" />
+            <IoIosArrowForward className="w-4 md:w-5 h-4 md:h-5 text-gray-a4a1aa group-disabled:text-gray-dddddd dark:group-disabled:text-gray-4b4b4b" />
           </button>
         </li>
       </ul>
