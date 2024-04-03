1ed276e5102f1fde9e2ef85282afc475e43c8a5a
ymj0828 <96277798+ymj0828@users.noreply.github.com>
2024-04-03 13:42:02 +0900

    feat: 로그인, 회원가입 페이지에 다크모튼 버튼 설정

--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -3,6 +3,7 @@ import { useForm, FormProvider } from "react-hook-form";
 import Router from "next/router";
 import Image from "next/image";
 import Link from "next/link";
+import { useTheme } from "next-themes";
 
 import { useLogin } from "@/hooks/auth";
 import { SignInModal } from "@/types/auth";
@@ -11,7 +12,11 @@ import PasswordInput from "@/components/common/AuthInput/PasswordInput";
 import AlertModal from "@/components/common/Modal/AlertModal";
 import Button from "@/components/common/Button/Button";
 
+import { Moon, Sun } from "lucide-react";
+
 const SignIn = () => {
+  const { theme, setTheme } = useTheme();
+
   const form = useForm({ mode: "onChange" });
 
   const [modal, setModal] = useState<SignInModal>({
@@ -41,54 +46,67 @@ const SignIn = () => {
   };
 
   return (
-    <FormProvider {...form}>
-      <div className="relative h-full min-w-[375px]">
-        <dialog ref={modal.fail} className="rounded-lg">
-          <AlertModal
-            type="alert"
-            size={modal.size}
-            text={modal.message}
-            handlerAlertModal={() => {
-              modal.fail.current.close();
-            }}
-          />
-        </dialog>
-        <div className="flex-col gap-5 mx-auto w-full pt-[80px] pb-[50px] px-[15px] md:w-[632px] md:pt-[150px] lg:w-[640px]">
-          <div>
-            <Image
-              src="/images/logo.png"
-              alt="logo"
-              width={300}
-              height={100}
-              onClick={() => {
-                Router.push("/");
+    <div>
+      <FormProvider {...form}>
+        <div className="relative h-full min-w-[375px]">
+          <dialog ref={modal.fail} className="rounded-lg">
+            <AlertModal
+              type="alert"
+              size={modal.size}
+              text={modal.message}
+              handlerAlertModal={() => {
+                modal.fail.current.close();
               }}
-              className="m-auto hover:cursor-pointer"
             />
-          </div>
-          <form
-            onSubmit={form.handleSubmit(submit.onSubmit, submit.onError)}
-            className="flex flex-col gap-3 mt-10"
-          >
+          </dialog>
+          <div className="flex-col gap-5 mx-auto w-full pt-[80px] pb-[50px] px-[15px] md:w-[632px] md:pt-[150px] lg:w-[640px]">
             <div>
-              <EmailInput whatFor="login" />
+              <Image
+                src="/images/logo.png"
+                alt="logo"
+                width={300}
+                height={94}
+                priority
+                onClick={() => {
+                  Router.push("/");
+                }}
+                className="m-auto hover:cursor-pointer"
+              />
             </div>
-            <div className="relative flex flex-col gap-1">
-              <PasswordInput whatFor="login" />
+            <form
+              onSubmit={form.handleSubmit(submit.onSubmit, submit.onError)}
+              className="flex flex-col gap-3 mt-10"
+            >
+              <div>
+                <EmailInput whatFor="login" />
+              </div>
+              <div className="relative flex flex-col gap-1">
+                <PasswordInput whatFor="login" />
+              </div>
+              <div className="mt-7">
+                <Button text="로그인 하기" size="full" type="submit" />
+              </div>
+            </form>
+            <div className="flex justify-center gap-3 text-sm mx-auto mt-8">
+              <p>회원이 아니신가요?</p>
+              <Link href="signup" className="underline">
+                회원가입하기
+              </Link>
             </div>
-            <div className="mt-7">
-              <Button text="로그인 하기" size="full" type="submit" />
-            </div>
-          </form>
-          <div className="flex justify-center gap-3 text-sm mx-auto mt-8">
-            <p>회원이 아니신가요?</p>
-            <Link href="signup" className="underline">
-              회원가입하기
-            </Link>
           </div>
         </div>
-      </div>
-    </FormProvider>
+      </FormProvider>
+      <button
+        className="fixed right-12 bottom-12 w-10 h-10 flex justify-center items-center rounded border border-white-ffffff bg-black"
+        type="button"
+        onClick={
+          theme === "dark" ? () => setTheme("light") : () => setTheme("dark")
+        }
+      >
+        <Sun className="text-white-ffffff hidden dark:block" />
+        <Moon className="text-white-ffffff block dark:hidden" />
+      </button>
+    </div>
   );
 };
 
--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -3,6 +3,7 @@ import { useForm, FormProvider } from "react-hook-form";
 import Router from "next/router";
 import Link from "next/link";
 import Image from "next/image";
+import { useTheme } from "next-themes";
 
 import { SignUpRequest, SignUpModal } from "@/types/auth";
 import { useUsers } from "@/hooks/useUsers";
@@ -13,7 +14,11 @@ import PasswordInput from "@/components/common/AuthInput/PasswordInput";
 import CheckPasswordInput from "@/components/common/AuthInput/CheckPasswordInput";
 import Button from "@/components/common/Button/Button";
 
+import { Moon, Sun } from "lucide-react";
+
 const SignUp = () => {
+  const { theme, setTheme } = useTheme();
+
   const form = useForm({ mode: "onChange" });
 
   const [modal, setModal] = useState<SignUpModal>({
@@ -49,65 +54,77 @@ const SignUp = () => {
   };
 
   return (
-    <FormProvider {...form}>
-      <div className="relative h-full min-w-[375px]">
-        <dialog ref={modal.success} className="rounded-lg">
-          <AlertModal type="alert" size={modal.size} text={modal.message} />
-        </dialog>
-        <dialog ref={modal.fail} className="rounded-lg">
-          <AlertModal
-            type="alert"
-            size={modal.size}
-            text={modal.message}
-            handlerAlertModal={() => {
-              modal.fail.current.close();
-            }}
-          />
-        </dialog>
-        <div className="flex-col gap-5 mx-auto w-full pt-[80px] pb-[50px] px-[15px] md:w-[632px] md:pt-[110px] lg:w-[640px]">
-          <div>
-            <Image
-              src="/images/logo.png"
-              alt="logo"
-              width={300}
-              height={100}
-              onClick={() => {
-                Router.push("/");
+    <div>
+      <FormProvider {...form}>
+        <div className="relative h-full min-w-[375px]">
+          <dialog ref={modal.success} className="rounded-lg">
+            <AlertModal type="alert" size={modal.size} text={modal.message} />
+          </dialog>
+          <dialog ref={modal.fail} className="rounded-lg">
+            <AlertModal
+              type="alert"
+              size={modal.size}
+              text={modal.message}
+              handlerAlertModal={() => {
+                modal.fail.current.close();
               }}
-              className="m-auto hover:cursor-pointer"
             />
+          </dialog>
+          <div className="flex-col gap-5 mx-auto w-full pt-[80px] pb-[50px] px-[15px] md:w-[632px] md:pt-[110px] lg:w-[640px]">
+            <div>
+              <Image
+                src="/images/logo.png"
+                alt="logo"
+                width={300}
+                height={94}
+                priority
+                onClick={() => {
+                  Router.push("/");
+                }}
+                className="m-auto hover:cursor-pointer"
+              />
+            </div>
+            <form
+              onSubmit={form.handleSubmit(submit.onSubmit, submit.onError)}
+              className="flex flex-col gap-3 mt-10"
+            >
+              <div>
+                <EmailInput whatFor="signUp" />
+              </div>
+              <div>
+                <NicknameInput />
+              </div>
+              <div>
+                <PasswordInput whatFor="signUp" />
+              </div>
+              <div>
+                <CheckPasswordInput whatFor="signUp" />
+              </div>
+              <div className="mt-7">
+                <Button text="회원가입 하기" size="full" type="submit"></Button>
+              </div>
+              <div className="flex justify-center gap-3 text-sm mx-auto mt-8">
+                <p>회원이신가요?</p>
+                <Link href="signin" className="underline">
+                  로그인하기
+                </Link>
+              </div>
+            </form>
           </div>
-          <form
-            onSubmit={form.handleSubmit(submit.onSubmit, submit.onError)}
-            className="flex flex-col gap-3 mt-10"
-          >
-            <div>
-              <EmailInput whatFor="signUp" />
-            </div>
-            <div>
-              <NicknameInput />
-            </div>
-            <div>
-              <PasswordInput whatFor="signUp" />
-            </div>
-            <div>
-              <CheckPasswordInput whatFor="signUp" />
-            </div>
-            <div className="mt-7">
-              <Button text="회원가입 하기" size="full" type="submit"></Button>
-            </div>
-            <div className="flex justify-center gap-3 text-sm mx-auto mt-8">
-              <p>회원이신가요?</p>
-              <Link href="signin" className="underline">
-                로그인하기
-              </Link>
-            </div>
-          </form>
         </div>
-      </div>
-    </FormProvider>
+      </FormProvider>
+      <button
+        className="fixed right-12 bottom-12 w-10 h-10 flex justify-center items-center rounded border border-white-ffffff bg-black"
+        type="button"
+        onClick={
+          theme === "dark" ? () => setTheme("light") : () => setTheme("dark")
+        }
+      >
+        <Sun className="text-white-ffffff hidden dark:block" />
+        <Moon className="text-white-ffffff block dark:hidden" />
+      </button>
+    </div>
   );
 };
 
 export default SignUp;
-
